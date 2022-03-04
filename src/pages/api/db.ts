b48220e00cdc55c098989ed3eb5c8282a671ec7f
MatCactus <lepluart.matteo@gmail.com>
2022-03-04 23:42:30 +0100

    Patch

--- src/pages/api/db.ts
+++ src/pages/api/db.ts
@@ -1,6 +1,6 @@
 // import { initializeApp, applicationDefault, cert } from "firebase-admin/app";
 // import { getFirestore, Timestamp, FieldValue } from "firebase-admin/firestore";
-// import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 
 // const serviceAccount = require(JSON.parse(process.env["GCLOUD_KEY"]));
 // initializeApp({
@@ -9,14 +9,12 @@
 // const db = getFirestore();
 // const collectionRef = db.collection("list");
 
-// export default async function onRequest(
-//     req: NextApiRequest,
-//     res: NextApiResponse
-// ) {
-//     const sendJson = (status: number, json: any) =>
-//         res.status(status).json(json);
-
-//     console.log(collectionRef.listDocuments);
-
-//     sendJson(200, {});
-// }
+export default async function onRequest(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    //     const sendJson = (status: number, json: any) =>
+    //         res.status(status).json(json);
+    //     console.log(collectionRef.listDocuments);
+    //     sendJson(200, {});
+}
